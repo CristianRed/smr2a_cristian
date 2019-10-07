09b2e1b9b6e337c29bb8fb54daf9c905e26ec7ca
CristianRed <cristianpereda1994@gmail.com>
2019-10-07 20:47:50 +0200

    fichero

--- fichero2a.php
+++ fichero2a.php
@@ -3,6 +3,7 @@ function Calculos($a,$b){
 	echo ($a+$b).'<br>';
 	echo ($a-$b).'<br>';
 	echo ($a*$b).'<br>';
-	echo ($a/$b).'<br>';
+	if($b!=0) echo ($a/$b).'<br>';
 }
 calculos(10,7);
+calculos(10,0);
---
+++ fichero2e.php
@@ -0,0 +1,14 @@
+<?php 
+function Calculos($a){
+	echo "<p>a x 1 ="; 				echo ($a*1).'</p>';
+	echo "<p>a x 1 =";				echo ($a*2).'</p>';
+	echo "<p>a x 1 =";				echo ($a*3).'</p>';
+	echo "<p>a x 1 =";				echo ($a*4).'</p>';
+	echo "<p>a x 1 =";				echo ($a*5).'</p>';
+	echo "<p>a x 1 =";				echo ($a*6).'</p>';
+	echo "<p>a x 1 =";				echo ($a*7).'</p>';
+	echo "<p>a x 1 =";				echo ($a*8).'</p>';
+	echo "<p>a x 1 =";				echo ($a*9).'</p>';
+	echo "<p>a x 1 =";				echo ($a*10).'</p>';
+}
+calculos(2);
